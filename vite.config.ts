19e59bebd95d5ed96104de143cb31f0f746e6d0b
Moataz Ghani <pss.alexandria@gmail.com>
2025-11-24 04:23:12 +0200

    Fix Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 }
+  root: process.cwd(),
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+  },
 })
